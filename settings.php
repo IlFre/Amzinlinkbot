b59b11b37f37acfd8a7c2cd43a599b4453a547bb
IlFre <72357099+IlFre@users.noreply.github.com>
2020-12-11 16:13:40 +0100

    Add files via upload

---
+++ settings.php
@@ -0,0 +1,7 @@
+<?php
+//IMPOSTAZIONI DA MODIFICARE
+$bitlytoken = 'f3b1cbc2793213e8acba8e6916b9f05b73f5bcff'; //inserisci qui dentro il token bitly [CAMBPO OBBLIGATORIO]
+$api = '1126963743:AAH8BksoV546c9NI85oPcBPQ-Cw90Z0yfAI'; //inserisci l'api token del tuo bot telegram [CAMBPO OBBLIGATORIO]
+$chats = [247324669]; //inserisci qui dentro i chatID che possono usare il bot se vuoi.
+$ref = 'ilfreamz00-21'; //inserisci qui dentro il tuo tag amazon.
+$amzn = false; //Metti true per referrare anche i link amzn.to (sconsigliato). Consigliato lasciare false.
